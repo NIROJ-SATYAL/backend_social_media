cb8ea4e82cc54bfb172ddc7db0dfb83cfced060d
niroj satyal <niroj.satyal2000@gmail.com>
2024-03-23 06:45:31 +0545

    added getEmailfromjwt method in jwtprovider class

--- src/main/java/com/basic/niroj/backend_social_media/Config/JwtProvider.java
+++ src/main/java/com/basic/niroj/backend_social_media/Config/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.basic.niroj.backend_social_media.Config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
@@ -11,16 +12,25 @@ import java.util.Date;
 public class JwtProvider {
 
 
- private SecretKey  key= Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+ private  static SecretKey  key= Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
 
- public String GenerateToken(Authentication auth)
+ public  static String GenerateToken(Authentication auth)
+ {
+     String jwt= Jwts.builder().setIssuer("niroj").setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis()+600000)).claim("email",auth.getName()).signWith(key).compact();
+     return jwt;
+ }
+
+
+ public static String getEmailFromToken(String token)
  {
 
+ String email=token.substring(7);
+
+ Claims claims=Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(email).getBody();
 
 
-     String jwt= Jwts.builder().setIssuer("niroj").setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis()+600000)).claim("email",auth.getName()).signWith(key).compact();
-
-        return null;
+return claims.get("email").toString();
+//return String.valueOf(claims.get("email")
  }
 }
